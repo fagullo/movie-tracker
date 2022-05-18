9212a7a680334ecf5c6f78d91e39fd46fab2fdf0
Paco Agullo <paco@square1.io>
2022-05-18 12:53:29 +0200

    Responsive Dashboard view.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="container mx-auto py-12 grow flex items-center">
-        <div class="w-full flex gap-6 items-center justify-center">
+        <div class="w-full flex gap-6 items-center justify-center flex-col md:flex-row px-4 md:px-0">
             <x-dashboard.movies-seen :views="$views" />
             <x-dashboard.trending-card :likes="$likes" />
         </div>
